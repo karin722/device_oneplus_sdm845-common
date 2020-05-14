cdd816c4edd919b8f6853de0a23fb064374ba8cf
firebird11 <hbgassel@gmail.com>
2020-05-14 11:26:54 +0200

    sdm845-common: DeviceSettings
    Change-Id: I7dd192534830ad927eafe83e77a9f7954095c75a
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -87,6 +87,10 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
@@ -166,7 +170,6 @@ PRODUCT_PACKAGES += \
 
 # tri-state-key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Trust HAL
